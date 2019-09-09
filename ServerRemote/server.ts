e6b13e108447c0ae62db8fb90e77292b84dad372
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-09-09 18:14:35 +0200

    Add project files.

---
+++ ServerRemote/server.ts
@@ -0,0 +1,7 @@
+import http = require('http');
+var port = process.env.port
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
