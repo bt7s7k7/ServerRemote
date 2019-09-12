833262a7f3dd79fb7ef88be0ae0102ad0bd47130
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-09-12 18:31:11 +0200

    Removed output files

--- ServerRemote/client/lib/app.js
+++
@@ -1,5 +0,0 @@
-import { E } from "./browserUtils";
-window.addEventListener("load", () => {
-    E.test.innerText = "it works";
-});
-//# sourceMappingURL=app.js.map
--- ServerRemote/messageTypes.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=messageTypes.js.map
